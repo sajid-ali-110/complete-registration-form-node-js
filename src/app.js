604662fc193c4f086d01d0e10eb49f3efe1c40a2
Sajid Ali <sajidaleekhan123123@gmail.com>
2025-07-21 23:53:42 +0500

    new ppages added

--- src/app.js
+++ src/app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const hbs = require("hbs");
+const bcrypt = require("bcrypt");
+const cookie = require("cookie");
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 3000;
+const jwt = require("jsonwebtoken")
+const auth = require("./middleware/auth")
 
 // DB Connection
 require("./db/conn");
@@ -18,6 +23,7 @@ const partialPath = path.join(__dirname, "../templates/partials");
 // Middleware
 app.use(express.static(staticPath));
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // View Engine Setup
 app.set("view engine", "hbs");
@@ -36,6 +42,10 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register");
 });
+app.get("/secret", auth, (req, res) => {
+  // console.log(`this is  awesome  ${req.cookies.jwt}`);
+  res.render("secret");
+});
 
 app.post("/register", async (req, res) => {
   try {
@@ -44,7 +54,7 @@ app.post("/register", async (req, res) => {
 
     if (password === cpassword) {
       const registeredEmployee = new Register({
-        firstName: req.body.firstname,   // match form field name
+        firstName: req.body.firstname, // match form field name
         lastName: req.body.lastname,
         email: req.body.email,
         gender: req.body.gender,
@@ -53,13 +63,23 @@ app.post("/register", async (req, res) => {
         password: req.body.password,
       });
 
+      console.log("the success part " + registeredEmployee);
+
+      const token = await registeredEmployee.generateAuthToken();
+      console.log("the token part " + token);
+
+      res.cookie("jwt", token, {
+        expires: new Date(Date.now() + 300000),
+        httpOnly: true,
+      });
+      console.log(cookie);
+
       const user = await registeredEmployee.save();
       console.log(user);
       res.status(201).render("index");
     } else {
       res.send("Passwords do not match");
     }
-
   } catch (e) {
     console.log("Error while registering:", e);
     res.status(500).send("Internal Server Error");
@@ -68,17 +88,35 @@ app.post("/register", async (req, res) => {
 
 // for login
 app.post("/login", async (req, res) => {
+  try {
     const email = req.body.email;
     const password = req.body.password;
 
-    const userEmail = await Register.findOne({email:email})
+    const userEmail = await Register.findOne({ email: email });
+    const isMatch = await bcrypt.compare(password, userEmail.password);
 
-    if (userEmail.password === password) {
-        res.status(201).render("index")
-    } else{
-        res.status(501).send("password is wrong")
+    const token = await userEmail.generateAuthToken();
+    console.log("the token part login " + token);
+
+    res.cookie("jwt", token, {
+      expires: new Date(Date.now() + 300000),
+      httpOnly: true,
+    });
+
+    if (isMatch) {
+      res.status(201).render("index");
+      console.log("logged in successfull");
+    } else {
+      res.status(501).send("password is wrong");
     }
+  } catch (error) {
+    console.log("invalid login details", error);
+  }
 });
+
+// hashing password
+
+// const saltRounds = 10;
 
 // Start Server
 app.listen(port, () => {
--- src/models/register.js
+++ src/models/register.js
@@ -1,36 +1,81 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const employeeSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    email: {
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  tokens: [
+    {
+      token: {
         type: String,
         required: true,
-        unique: true
+      },
     },
-    gender: {
-        type: String,
-        required: true
-    },
-    phone: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    age: {
-        type: Number,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
+  ],
+});
+
+// generating tokens
+
+employeeSchema.methods.generateAuthToken = async function () {
+  try {
+    const token = jwt.sign(
+      { _id: this._id.toString() },
+      process.env.SECRET_KEY
+    );
+    this.tokens = this.tokens.concat({ token });
+    await this.save();
+    return token;
+  } catch (error) {
+    console.log("Token generation error:", error);
+    throw error;
+  }
+};
+
+// convert password into hash
+
+employeeSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    try {
+      const hashedPassword = await bcrypt.hash(this.password, 10);
+      this.password = hashedPassword;
+      next();
+    } catch (err) {
+      next(err);
     }
+  } else {
+    next();
+  }
 });
 
 const Register = mongoose.model("Register", employeeSchema);
